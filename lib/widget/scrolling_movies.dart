fac5f6d17beab5f340c995e135f152a7ec3b925d
Ini Jones <jonesinim@gmail.com>
2022-05-14 22:58:29 +0100

    Added the Scrolling Movies widget

---
+++ lib/widget/scrolling_movies.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_provider/constant/api_constants.dart';
+import 'package:movie_app_provider/models/functions.dart';
+import 'package:movie_app_provider/models/genre/genre.dart';
+import 'package:movie_app_provider/models/movie/movie.dart';
+
+class ScrollingMovies extends StatefulWidget {
+  final ThemeData themeData;
+  final String? api, title;
+  final List<Genres> genre;
+  const ScrollingMovies({
+    Key? key,
+    required this.themeData,
+    this.api,
+    this.title,
+    required this.genre,
+  }) : super(key: key);
+
+  @override
+  State<ScrollingMovies> createState() => _ScrollingMoviesState();
+}
+
+class _ScrollingMoviesState extends State<ScrollingMovies> {
+  List<Movie>? moviesList;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies(widget.api!).then(
+      (value) => {
+        setState(
+          () => moviesList = value,
+        ),
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            widget.title!,
+            style: widget.themeData.textTheme.headline5,
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 200,
+          child: moviesList == null
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Hero(
+                            tag: '${moviesList![index].id}${widget.title}',
+                            child: SizedBox(
+                              width: 100,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: FadeInImage(
+                                        image: NetworkImage(
+                                            TMDB_BASE_IMAGE_URL +
+                                                'w500/' +
+                                                moviesList![index].posterPath!),
+                                        fit: BoxFit.cover,
+                                        placeholder: const AssetImage(
+                                            'assets/images/loading.gif'),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      moviesList![index].title!,
+                                      style:
+                                          widget.themeData.textTheme.bodyText1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )),
+                      ),
+                    );
+                  },
+                ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widget/search_movies_widget.dart
+++ lib/widget/search_movies_widget.dart
@@ -41,87 +41,90 @@ class _SearchMovieWidgetState extends State<SearchMovieWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: widget.themeData!.primaryColor,
-        child: moviesList == null
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : moviesList!.isEmpty
-                ? Center(
-                    child: Text(
-                      "Oops! couldn't find the movie",
-                      style: widget.themeData!.textTheme.bodyText1,
-                    ),
-                  )
-                : ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: moviesList!.length,
-                    itemBuilder: (BuildContext context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: 70,
-                                    height: 80,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child:
-                                          moviesList![index].posterPath == null
-                                              ? Image.asset(
-                                                  'assets/image/na.jpg',
-                                                  fit: BoxFit.cover,
-                                                )
-                                              : FadeInImage(
-                                                  placeholder: const AssetImage(
-                                                      'assets/images/loading.gif'),
-                                                  image: NetworkImage(
-                                                      TMDB_BASE_IMAGE_URL +
-                                                          'w500/' +
-                                                          moviesList![index]
-                                                              .posterPath!),
-                                                  fit: BoxFit.cover,
-                                                ),
+      color: widget.themeData!.primaryColor,
+      child: moviesList == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : moviesList!.isEmpty
+              ? Center(
+                  child: Text(
+                    "Oops! couldn't find the movie",
+                    style: widget.themeData!.textTheme.bodyText1,
+                  ),
+                )
+              : ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: moviesList!.length,
+                  itemBuilder: (BuildContext context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 70,
+                                  height: 80,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: moviesList![index].posterPath == null
+                                        ? Image.asset(
+                                            'assets/image/na.jpg',
+                                            fit: BoxFit.cover,
+                                          )
+                                        : FadeInImage(
+                                            placeholder: const AssetImage(
+                                                'assets/images/loading.gif'),
+                                            image: NetworkImage(
+                                                TMDB_BASE_IMAGE_URL +
+                                                    'w500/' +
+                                                    moviesList![index]
+                                                        .posterPath!),
+                                            fit: BoxFit.cover,
+                                          ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          moviesList![index].voteAverage!,
+                                          style: widget
+                                              .themeData!.textTheme.bodyText1,
+                                        ),
+                                        const Icon(
+                                          Icons.star,
+                                          color: Colors.green,
+                                        )
+                                      ],
                                     ),
                                   ),
-                                  Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            moviesList![index].voteAverage!,
-                                            style: widget
-                                                .themeData!.textTheme.bodyText1,
-                                          ),
-                                          const Icon(
-                                            Icons.star,
-                                            color: Colors.green,
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                                child: Divider(color: widget.themeData!.colorScheme.secondary),
-                              )
-                            ],
-                          ),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 24.0),
+                              child: Divider(
+                                  color:
+                                      widget.themeData!.colorScheme.secondary),
+                            )
+                          ],
                         ),
-                      );
-                    },
-                  ));
+                      ),
+                    );
+                  },
+                ),
+    );
   }
 }
